9f351b165c1ef89c46d859fb1973904850515bb7
JamMiks <artemagarkov679@gmail.com>
2026-06-11 10:02:57 +0300

    Answer

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace учение
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string answer = "Нет";
+            if(answer == "Да")
+            {
+                Console.WriteLine("Продолжаем");
+            }
+            else
+            {
+                Console.WriteLine("Стоп");
+            }
+        } 
+         
+
+
+
+
+
+
+    }
+}
